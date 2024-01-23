0f2ae39e57fe7092a78ff1b6d05b17fc76ff2c7f
gustcomer <gustcomer@gmail.com>
2024-01-23 10:56:05 -0300

    feat: add 'Add todo' text input and submit button. Put them inside a form.

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ class View {
     this.app = document.querySelector('#root')
     this.title = document.createElement('h1')
     this.title.textContent = 'Hello World'
-    this.app.append(this.title)
+    this.form = document.createElement('form')
+    this.input = document.createElement('input')
+    this.input.type = 'text'
+    this.input.placeholder = 'Add todo'
+    this.input.name = 'todo'
+    this.submitButton = document.createElement('button')
+    this.submitButton.textContent = 'Submit'
+    this.form.append(this.input, this.submitButton)
+    this.app.append(this.title, this.form)
   }
 }
 
